ecc3c0704bff1e0efb7f90b61d93e0bf15812772
Alexander Halpern <halperna22@gmail.com>
2023-12-08 17:12:18 +0000

    updates

--- Student/lecture_15.lean
+++ Student/lecture_15.lean
@@ -1,26 +1,26 @@
 /-!
 # Satifiability Modulo Theories
 
-At the end of the last chapter, we saw first-hand the 
-magnificence of automated satisfiability and validity 
+At the end of the last chapter, we saw first-hand the
+magnificence of automated satisfiability and validity
 checking for propositional logic. Most recently we met
 model-finding algorithms. As usual lately, this chapter
 starts by presenting an updated and compressed version
 of our specifications for proposition logic, properties
 of expressions, and model finding. We'll briefly review
-this material at the start of class. We'll then turn to 
-our main new topic: *satisfiability modulo theories*. 
+this material at the start of class. We'll then turn to
+our main new topic: *satisfiability modulo theories*.
 -/
 
 /-!
 ## Review and Extensions
 -/
 
-/-! 
+/-!
 Higher-order functions in lists
 -/
 #check @List.map
-#check @List.foldr 
+#check @List.foldr
 #check @List.filter
 
 /-!
@@ -34,17 +34,17 @@ inductive binary_op : Type
 | imp
 | iff
 inductive Expr : Type
-| true_exp 
-| false_exp 
+| true_exp
+| false_exp
 | var_exp (v : var)
 | un_exp (op : unary_op) (e : Expr)
 | bin_exp (op : binary_op) (e1 e2 : Expr)
 notation "{"v"}" => Expr.var_exp v
-prefix:max "¬" => Expr.un_exp unary_op.not 
-infixr:35 " ∧ " => Expr.bin_exp binary_op.and  
-infixr:30 " ∨ " => Expr.bin_exp binary_op.or 
+prefix:max "¬" => Expr.un_exp unary_op.not
+infixr:35 " ∧ " => Expr.bin_exp binary_op.and
+infixr:30 " ∨ " => Expr.bin_exp binary_op.or
 infixr:25 " ⇒ " =>  Expr.bin_exp binary_op.imp
-infixr:20 " ⇔ " => Expr.bin_exp binary_op.iff 
+infixr:20 " ⇔ " => Expr.bin_exp binary_op.iff
 notation " ⊤ " => Expr.top_exp
 notation " ⊥ " => Expr.bot_exp
 
@@ -63,8 +63,8 @@ def eval_bin_op : binary_op → (Bool → Bool → Bool)
 | binary_op.or => or
 | binary_op.imp => implies
 | binary_op.iff => iff
-def Interp := var → Bool 
-def eval_expr : Expr → Interp → Bool 
+def Interp := var → Bool
+def eval_expr : Expr → Interp → Bool
 | Expr.true_exp,           _ => true
 | Expr.false_exp,          _ => false
 | (Expr.var_exp v),        i => i v
@@ -79,35 +79,35 @@ def reduce_or := List.foldr or false
 def reduce_and := List.foldr and true
 def make_bool_lists: Nat → List (List Bool)
 | 0 => [[]]
-| n + 1 =>  (List.map (fun L => false::L) (make_bool_lists n)) ++ 
+| n + 1 =>  (List.map (fun L => false::L) (make_bool_lists n)) ++
             (List.map (fun L => true::L) (make_bool_lists n))
 def override : Interp → var → Bool → Interp
-| old_interp, var, new_val => 
-  (λ v => if (v.n == var.n)   
-          then new_val        
-          else old_interp v)  
+| old_interp, var, new_val =>
+  (λ v => if (v.n == var.n)
+          then new_val
+          else old_interp v)
 def bool_list_to_interp : List Bool → Interp
   | l => bools_to_interp_helper l.length l
 where bools_to_interp_helper : (vars : Nat) → (vals : List Bool) → Interp
   | _, [] => (λ _ => false)
   | vars, h::t =>
     let len := (h::t).length
-    override (bools_to_interp_helper vars t) (var.mk (vars - len)) h 
+    override (bools_to_interp_helper vars t) (var.mk (vars - len)) h
 def interp_to_list_bool : (num_vars : Nat) → Interp →  List Bool
 | 0, _ => []
 | (n' + 1) , i => interp_to_list_bool n' i ++ [(i (var.mk n'))]
-def interps_to_list_bool_lists : Nat → List Interp → List (List Bool) 
+def interps_to_list_bool_lists : Nat → List Interp → List (List Bool)
 | vars, is => List.map (interp_to_list_bool vars) is
 def max_variable_index : Expr → Nat
   | Expr.true_exp => 0
   | Expr.false_exp => 0
   | Expr.var_exp (var.mk i) => i
   | Expr.un_exp _ e => max_variable_index e
-  | Expr.bin_exp _ e1 e2 => max (max_variable_index e1) (max_variable_index e2) 
+  | Expr.bin_exp _ e1 e2 => max (max_variable_index e1) (max_variable_index e2)
 def mk_interps_vars : Nat → List Interp
-| n => List.map bool_list_to_interp (make_bool_lists n) 
+| n => List.map bool_list_to_interp (make_bool_lists n)
 -- main api
-def num_vars : Expr → Nat := λ e => max_variable_index e + 1                    
+def num_vars : Expr → Nat := λ e => max_variable_index e + 1
 def mk_interps_expr : Expr → List Interp
 | e => mk_interps_vars (num_vars e)
 def truth_table_outputs : Expr → List Bool
@@ -133,8 +133,8 @@ def find_model : Expr → Option Interp
 where find_model_helper : List Interp → Expr → Option Interp
 | [], _ => none
 | h::t, e => if (eval_expr e h) then some h else find_model_helper t e
-def find_models (e : Expr) := 
-  List.filter                 -- filter on 
+def find_models (e : Expr) :=
+  List.filter                 -- filter on
     (λ i => eval_expr e i)    -- i makes e true
     (mk_interps_expr e)       -- over all interps
 def find_models_bool : Expr → List (List Bool)
@@ -154,14 +154,14 @@ def Z := {var.mk 2}
 
 -- If X being true makes Y true, then does X being false make Y false?
 #check ((X ⇒ Y) ⇒ (¬X ⇒ ¬Y))
-#eval is_valid ((X ⇒ Y) ⇒ (¬X ⇒ ¬Y))    
-#reduce find_counterexamples_bool ((X ⇒ Y) ⇒ (¬X ⇒ ¬Y)) 
+#eval is_valid ((X ⇒ Y) ⇒ (¬X ⇒ ¬Y))
+#reduce find_counterexamples_bool ((X ⇒ Y) ⇒ (¬X ⇒ ¬Y))
 #reduce (implies (implies false true) (implies true false))
 
 -- If X implies Y, then does not Y false imply not X?
 #check ((X ⇒ Y) ⇒ (¬Y ⇒ ¬X))
-#eval is_valid ((X ⇒ Y) ⇒ (¬Y ⇒ ¬X))    
-#reduce find_counterexamples_bool ((X ⇒ Y) ⇒ (¬Y ⇒ ¬X)) 
+#eval is_valid ((X ⇒ Y) ⇒ (¬Y ⇒ ¬X))
+#reduce find_counterexamples_bool ((X ⇒ Y) ⇒ (¬Y ⇒ ¬X))
 
 -- Find all the models of an expression.
 #eval find_models_bool ((X ⇒ Y) ⇒ (¬X ⇒ ¬Y))
@@ -178,7 +178,7 @@ def Z := {var.mk 2}
 -- Find all models, returns list of bool lists
 #eval find_models_bool (X ∧ ¬ X)                     -- []
 #eval find_models_bool (X ∨ ¬ X)                     -- [[false], [true]
-#eval find_models_bool (X ∧ Y)                       -- [[true, true]]  
+#eval find_models_bool (X ∧ Y)                       -- [[true, true]]
 #eval find_models_bool (¬(X ∧ Y) ⇒ ¬X ∨ ¬Y)          -- all four interps
 #eval find_models_bool ((X ⇒ Y) ⇒ (Y ⇒ Z) ⇒ (X ⇒ Z)) -- all eight interps
 
@@ -190,23 +190,23 @@ If you're impressed that we can have software that
 automatically solves systems of Boolean constraint, you
 will be especially delighted to learn that SMT solvers
 also solve systems where atomic propositions can be
-elaborated in other formal languages, such as that 
-of everyday arithmetic. Solvers are now need to handle 
-things like systems of linear inequalities. 
+elaborated in other formal languages, such as that
+of everyday arithmetic. Solvers are now need to handle
+things like systems of linear inequalities.
 
 In the language of a typical SMT solver, like DeMoura's
-Z3, atomic propositions in propositional logic can be 
-expanded into formulas in a variety of other formal 
-languages, e.g., the language of linear inequalities, 
+Z3, atomic propositions in propositional logic can be
+expanded into formulas in a variety of other formal
+languages, e.g., the language of linear inequalities,
 for which fully automated solvers, that can find values
-for arithmetic variables that make expressions true, 
+for arithmetic variables that make expressions true,
 do exist.
 
-So, whereas in propositional logic, you can write *X ∧ Y*, 
-in propositional logic extended with the "theory" called 
-*everyday arithmetic*, you can write (X > Y + 2) ∧ (Y ≤ 7). 
+So, whereas in propositional logic, you can write *X ∧ Y*,
+in propositional logic extended with the "theory" called
+*everyday arithmetic*, you can write (X > Y + 2) ∧ (Y ≤ 7).
 
-Question: You solve it! What's the smallest integer value of 
+Question: You solve it! What's the smallest integer value of
 X for which there's some value of Y so that together X and Y
 satisfy the overall formula?
 
